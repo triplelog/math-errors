39b0e21c2f01281fc334cf11f6060be4e2ebe563
triplelog <robmwilc@gmail.com>
2020-03-26 19:02:52 -0400

    First

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -81,7 +81,7 @@ void MethodAddWrong(const Nan::FunctionCallbackInfo<v8::Value>& info) {
     }
     else {
     	typedef std::string (*hello_t)(std::vector<std::string>, std::string);
-		hello_t hello = (hello_t) dlsym(handle, "addIntsWrongTest");
+		hello_t hello = (hello_t) dlsym(handle, "addIntsWrongSO");
 		out = hello(strs,str3);
     }
 	
