59d570bb6b6f674d624aedcaffa4a72581d6139d
triplelog <robmwilc@gmail.com>
2020-03-26 18:56:38 -0400

    First

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -80,7 +80,7 @@ void MethodAddWrong(const Nan::FunctionCallbackInfo<v8::Value>& info) {
         out = "no handle";
     }
     else {
-    	typedef std::string (*hello_t)();
+    	typedef std::string (*hello_t)(std::string);
 		hello_t hello = (hello_t) dlsym(handle, "addIntsWrongTest");
 		out = hello("newer");
     }
