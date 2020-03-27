4c1bdbacb8cc21ee5ca6ab2e558086f02d1bce58
triplelog <robmwilc@gmail.com>
2020-03-27 00:08:36 -0400

    First

--- cpp/arithmetic.cpp
+++ cpp/arithmetic.cpp
@@ -25,9 +25,9 @@
 #include <array>
 
 extern "C" {
-#include "lua/lualib.h"
-#include "lua/lauxlib.h"
-#include "lua/lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+#include "lua.h"
 }
 
 inline std::string digitsToString(std::vector<short> digits, bool order){
