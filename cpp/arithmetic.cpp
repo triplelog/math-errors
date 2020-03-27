0f5a6fdff4200a9d9e7a4daec41845e22d7d0ec7
triplelog <robmwilc@gmail.com>
2020-03-27 09:06:06 -0400

    First

--- cpp/arithmetic.cpp
+++ cpp/arithmetic.cpp
@@ -197,7 +197,7 @@ extern "C" std::string addIntsWrongSO(std::vector<std::string> strs, std::string
 			for (ii=0;ii<digits.size();ii++){
 				if (i < digits[ii].size()){
 					newdigit = digit + digits[ii][i];
-					if (newdigit/10 > digit/10 && rand() % 1000 > 970){
+					if (newdigit/10 > digit/10 && iii % 1000 > 970){
 						digit = newdigit - 10;
 						std::string d(1,i+'2'); //next digit will be wrong, and start at 1 not 0
 						errors += "You missed a carry on "+d+"rd digit from right.\n";
