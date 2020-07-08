ae1125254999914a13dc1a01d8c85a6af2a96c26
triplelog <robmwilc@gmail.com>
2020-07-08 08:04:45 -0400

    first

---
+++ cpp/postfix.cpp
@@ -0,0 +1,271 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <math.h>
+#include <limits.h>
+#include <time.h>
+#include <string.h> 
+#include <ctype.h>
+#include <cmath>
+#include <iostream>
+#include <variant>
+#include <map>
+#include <numeric>
+#include <chrono>
+#include <thread>
+#include <sstream>
+#include <iostream>
+#include <array>
+#include "parallel_hashmap/phmap.h"
+
+
+using namespace std::chrono;
+using phmap::flat_hash_map;
+flat_hash_map<std::string,std::string> treeMap;
+flat_hash_map<char,int> prec;
+prec['*'] = 4;
+prec['/'] = 4;
+prec['+'] = 3;
+prec['~'] = 3;
+prec['>'] = 2;
+prec['<'] = 2;
+prec['='] = 2;
+prec['!'] = 2;
+prec['['] = 2;
+prec[']'] = 2;
+prec['&'] = 1;
+prec['|'] = 0;
+prec['('] = -1;
+prec[')'] = -1;
+
+std::string makePost(char infixexpr[]) {
+	/*opStack = []
+	postfixList = []
+	intstr = ''
+	expstr = ''*/
+	std::vector<std::string> tokenList;
+	int idx = 0;
+	std::string temptoken = "";
+	for (i = 0; infixexpr[i]; i++) 
+    {
+		var ie = infixexpr[i];
+		if (prec.find(ie) == prec.end()){
+			temptoken += ie;
+		}
+		else{
+			if (temptoken != ""){
+				tokenList[idx] = temptoken;
+				idx++;
+			}
+			std::string s(1,ie);
+			tokenList[idx] = s;
+			idx++;
+			temptoken = "";
+		}
+	}
+	if (temptoken != ""){
+		tokenList[idx] = temptoken;
+	}
+	for (std::vector<int>::iterator it = myvector.begin() ; it != myvector.end(); ++it)
+	return "tokenList";
+	/*
+	for (var i=0;i<tokenList.length;i++){
+		var token = tokenList[i];
+		if ("*+/~><=![]&|".indexOf(token) == -1){
+			postfixList.push(token)
+		}
+		else if (token == '('){
+			opStack.push(token)
+		}
+		else if (token == ')'){
+			topToken = opStack.pop()
+			while (topToken != '('){
+				postfixList.push(topToken)
+				topToken = opStack.pop()
+			}
+		}
+		else {
+			while ((opStack.length > 0) && (prec[opStack[opStack.length-1]] >= prec[token])){
+				postfixList.push(opStack.pop())
+			}
+			opStack.push(token)
+		}
+	}
+	while (opStack.length > 0){
+		postfixList.push(opStack.pop())
+	}
+	for (var i=0;i<postfixList.length;i++){
+		var ci = postfixList[i];
+		if ("*+/~><=![]&|".indexOf(ci) == -1){
+			intstr += ci + '_'
+			expstr += '#'
+		}
+		else if (ci == '~'){
+			expstr += '-'
+		}
+		else{
+			expstr += ci
+		}
+	}
+	intstr = intstr.substring(0,intstr.length-1)
+	
+	return [intstr,expstr]
+	*/
+
+}
+
+/*
+function replaceDecimals(istr){
+	dindex = istr.indexOf('.');
+	while (dindex >-1){
+		intpart = 0;
+		decpart = 0;
+		denom = 1;
+		strparts = [dindex,dindex+1];
+		for (var i=1;i<dindex+1;i++){
+			if ("0123456789".indexOf(istr[dindex-i]) > -1){
+				intpart += parseInt(istr[dindex-i])*Math.pow(10,i-1);
+				strparts[0] = dindex-i;
+			}
+			else{break;}
+		}
+		for (var i=dindex+1;i<istr.length;i++){
+			if ("0123456789".indexOf(istr[i]) > -1){
+				decpart *=10;
+				denom *=10;
+				decpart += parseInt(istr[i]);
+				strparts[1] = i+1;
+			}
+			else{break;}
+		}
+		istr = istr.substring(0,strparts[0])+'('+ (intpart*denom+decpart) +'/'+ denom +')'+istr.substring(strparts[1],);
+		dindex = istr.indexOf('.');
+	}
+
+	return istr
+}
+
+function replaceNegatives(istr){
+	dindex = istr.indexOf('-')
+	while (dindex >-1){
+		if (dindex == 0){
+			if ("0123456789".indexOf(istr[1]) == -1) {
+				istr = '-1*'+istr.substring(1,);
+			}
+			dindex = istr.indexOf('-',1);
+		}
+		else{
+			if ("><=![]&|(".indexOf(istr[dindex-1])> -1) {
+				if ("0123456789".indexOf(istr[dindex-1])== -1){
+					istr = istr.substring(0,dindex)+'-1*'+istr.substring(dindex+1,);
+				}
+				dindex = istr.indexOf('-',dindex+1);
+			}
+			else{
+				istr = istr.substring(0,dindex)+'~'+istr.substring(dindex+1,);
+				dindex = istr.indexOf('-',dindex+1);
+			}
+		}
+	}
+				
+	return istr
+}
+*/
+
+std::string postfixify(std::string input_str) {
+	/*input_str = input_str.toUpperCase();
+	input_str = input_str.replace(/AND/g,'&');
+	input_str = input_str.replace(/OR/g,'|');
+	input_str = input_str.replace(/\[/g,'(');
+	input_str = input_str.replace(/]/g,')');
+	input_str = input_str.replace(/{/g,'(');
+	input_str = input_str.replace(/}/g,')');
+	input_str = input_str.replace(/>=/g,']');
+	input_str = input_str.replace(/<=/g,'[');
+	input_str = input_str.replace(/==/g,'=');
+	input_str = input_str.replace(/!=/g,'!');
+	input_str = input_str.replace(/\+-/g,'-');
+	input_str = input_str.replace(/--/g,'+');
+	input_str = replaceDecimals(input_str);
+	input_str = replaceNegatives(input_str);*/
+	
+	
+	//var twoparts = makePost(input_str);
+	
+	
+	/*
+	//Convert column names
+	console.log(twoparts[0]);
+	console.log(twoparts[1]);
+	var firstpart = twoparts[0].split("_");
+	for (var i=0;i<firstpart.length;i++){
+		if (parseInt(firstpart[i]).toString() != firstpart[i]){
+			for (var ii in colInfo) {
+				if (colInfo[ii].toUpperCase() == firstpart[i]) {
+					firstpart[i] = 'c'+ii;
+					break;
+				}
+			}
+		}
+		else {
+			firstpart[i] = firstpart[i]+'.1.I';
+		}
+	}
+	var fullstr = firstpart.join("_")+'@'+twoparts[1];
+	*/
+	return "fullstr";
+}
+
+/*
+inline Cppdata solvePostfixVV(char expstr[], std::vector<Cppdata> const intArray, std::vector<Cppdata> stack)
+{
+
+	int i;
+  	int currentIndex = 0;
+  	int arrayIndex = 0;
+
+    for (i = 0; expstr[i]; i++) 
+    { 
+        if (expstr[i] == '#') {
+        	stack[currentIndex] = intArray[arrayIndex];
+        	currentIndex++;
+        	arrayIndex++;
+  
+        } else 
+        { 
+            switch (expstr[i]) 
+            { 
+	            case '>': stack[currentIndex - 2].w = (stack[currentIndex - 2] > stack[currentIndex - 1]) ? 1 : 0; stack[currentIndex - 2].t = 'B'; break; 
+	            case '<': stack[currentIndex - 2].w = (stack[currentIndex - 2] < stack[currentIndex - 1]) ? 1 : 0; stack[currentIndex - 2].t = 'B'; break;
+	            case ']': stack[currentIndex - 2].w = (stack[currentIndex - 2] >= stack[currentIndex - 1]) ? 1 : 0; stack[currentIndex - 2].t = 'B'; break; 
+	            case '[': stack[currentIndex - 2].w = (stack[currentIndex - 2] <= stack[currentIndex - 1]) ? 1 : 0; stack[currentIndex - 2].t = 'B'; break;
+	            case '+': stack[currentIndex - 2] = stack[currentIndex - 2] + stack[currentIndex - 1]; break; 
+	            case '-': stack[currentIndex - 2] = stack[currentIndex - 2] - stack[currentIndex - 1]; break; 
+	            case '*': stack[currentIndex - 2] = stack[currentIndex - 2] * stack[currentIndex - 1]; break; 
+	            case '/': stack[currentIndex - 2] = stack[currentIndex - 2] / stack[currentIndex - 1]; break;
+	            case '=': stack[currentIndex - 2] = stack[currentIndex - 2] == stack[currentIndex - 1]; break;
+	            case '!': stack[currentIndex - 2] = stack[currentIndex - 2] != stack[currentIndex - 1]; break;
+	            //case '%': stack[currentIndex - 2] = stack[currentIndex - 2] % stack[currentIndex - 1]; break; 
+	            case '&': stack[currentIndex - 2].w = (stack[currentIndex - 2].w + stack[currentIndex - 1].w > 1) ? 1 : 0; stack[currentIndex - 2].t = (stack[currentIndex - 2].t == 'B' && stack[currentIndex - 1].t == 'B') ? 'B' : 'N'; break; 
+	            case '|': stack[currentIndex - 2].w = (stack[currentIndex - 2].w + stack[currentIndex - 1].w == 0) ? 0 : 1; stack[currentIndex - 2].t = (stack[currentIndex - 2].t == 'B' && stack[currentIndex - 1].t == 'B') ? 'B' : 'N'; break; 
+	            //multiandcase '&': if (stack[currentIndex - 5] > 0 && stack[currentIndex - 4] > 0 && stack[currentIndex - 3] > 0 && stack[currentIndex - 2] > 0 && stack[currentIndex - 1] > 0) {stack[currentIndex - 5] = 1;} else {stack[currentIndex - 5] = -1;}; currentIndex--; currentIndex--; currentIndex--; currentIndex--; break; 
+            
+            } 
+            currentIndex--;
+        } 
+    } 
+
+
+
+	return stack[0];
+
+}
+*/
+
+int main () {
+	char[3] infixexpr;
+	infixexpr[0] = 'a';
+	infixexpr[1] = '+';
+	infixexpr[2] = 'b';
+	makePost(infixexpr);
+}
