b4aa60e2a3335fcdf9e0ebbb406043c95204d879
triplelog <robmwilc@gmail.com>
2020-07-09 13:12:25 -0400

    first

--- cpp/postfix.cpp
+++ cpp/postfix.cpp
@@ -1319,9 +1319,9 @@ int main () {
 		std::vector<std::string> postList = makeTree(oldPostfix);
 	
 		for (ii=0;ii<postList.size();ii++){
-			std::cout << "--------\n" << ii << " ---- " << postList[ii] << '\n--------------';
+			std::cout << "--------\n" << ii << " ---- " << postList[ii] << "\n--------------";
 			newPostfix = applyRules(postList[ii]);
-			std::cout << "--------\n" << ii << " ---- " << postList[ii] << '\n--------------';
+			std::cout << "--------\n" << ii << " ---- " << postList[ii] << "\n--------------";
 			if (newPostfix != postList[ii]){
 				std::cout << "MatchT: " << postList[ii] << " into "<< newPostfix << " from " << oldPostfix << '\n';
 				break;
