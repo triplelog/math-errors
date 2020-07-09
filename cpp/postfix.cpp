e08943959c9a3939356f8c49376e26f6f5055e47
triplelog <robmwilc@gmail.com>
2020-07-08 23:40:12 -0400

    first

--- cpp/postfix.cpp
+++ cpp/postfix.cpp
@@ -652,7 +652,7 @@ std::vector<std::string> makeRule(std::string input){
 flat_hash_map<std::string,std::vector<std::string>> makeRules(){
 	flat_hash_map<std::string,std::vector<std::string>> finalRules;
 	std::vector<std::vector<std::string>> rawRules;
-	rawRules.push_back({"ddx(x^3+B)","ddx(x^3)+ddx(B)","Sum Rule."});
+	rawRules.push_back({"ddx(A+B)","ddx(A)+ddx(B)","Sum Rule."});
 	rawRules.push_back({"ddx(x^3)","3*x^2","Turn exponent into multiplication."});
 	rawRules.push_back({"ddx(5)","0","Turn exponent into multiplication."});
 	int i; int ii;
