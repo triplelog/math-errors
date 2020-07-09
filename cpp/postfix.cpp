8d4f6bc520a207a1c8c286ba6b6496bccdaa1a71
triplelog <robmwilc@gmail.com>
2020-07-09 13:14:29 -0400

    first

--- cpp/postfix.cpp
+++ cpp/postfix.cpp
@@ -1321,11 +1321,12 @@ int main () {
 		for (ii=0;ii<postList.size();ii++){
 			std::cout << "--------\n" << ii << " ---- " << postList[ii] << "\n--------------";
 			newPostfix = applyRules(postList[ii]);
-			std::cout << "--------\n" << ii << " ---- " << postList[ii] << "\n--------------";
+			std::cout << "--------\n" << ii << " ---- " << newPostfix << "\n--------------";
 			if (newPostfix != postList[ii]){
 				std::cout << "MatchT: " << postList[ii] << " into "<< newPostfix << " from " << oldPostfix << '\n';
 				break;
 			}
+			std::cout << "--------\n" << ii << " ---- " << newPostfix << "\n--------------";
 			
 		}
 		std::cout << ii << "-:-" << postList[ii] << '\n';
