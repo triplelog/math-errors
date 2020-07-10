79e2eec42597449f2f98fa48c21962969d96ecee
triplelog <robmwilc@gmail.com>
2020-07-10 10:02:26 -0400

    first

--- cpp/postfix.cpp
+++ cpp/postfix.cpp
@@ -714,6 +714,12 @@ std::vector<std::string> makeTree(std::string pfstr){
 	}*/
 	for (ii=orderedKeyList.size()-1;ii>=0;ii--){
 		std::string outText = "Node: " + orderedKeyList[ii] + " and " + nodeList[orderedKeyList[ii]][0] + " and " + nodeList[orderedKeyList[ii]][1];
+		
+		std::string outText = nodeList[orderedKeyList[ii]][0] + " = {\n";
+		if (nodeList[orderedKeyList[ii]][1].length() > 0){
+			outText += "parent: "+nodeList[orderedKeyList[ii]][1] + ",\n";
+		}
+		outText += "text: { name: \"" + orderedKeyList[ii] + "\" }\n};";
 		std::cout << outText << "\n";
 	}
 	
