1af4aa475c6fc41b1ec04ae7fc881ec57904c18c
triplelog <robmwilc@gmail.com>
2020-07-10 14:50:32 -0400

    first

---
+++ main-server.js
@@ -0,0 +1,89 @@
+'use strict';
+const { PerformanceObserver, performance } = require('perf_hooks');
+var fs = require("fs");
+const assert = require('assert');
+const binding = require.resolve(`./build/Release/binding`);
+const maincpp = require(binding);
+//const postfix = require('./postfix.js');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/matherrors', {useNewUrlParser: true});
+var fromLogin = require('./login-server.js');
+var app = fromLogin.loginApp;
+var tempKeys = fromLogin.tempKeys;
+
+
+const https = require('https');
+var fs = require("fs");
+var myParser = require("body-parser");
+var qs = require('querystring');
+const { exec } = require('child_process');
+var nunjucks = require('nunjucks');
+var crypto = require("crypto");
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/matherrors.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/matherrors.com/fullchain.pem')
+};
+
+
+
+
+const User = require('./models/user');
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
+// use static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy());
+ 
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+var express = require('express');
+
+app.use('/',express.static('static'));
+
+
+const server1 = https.createServer(options, app);
+
+server1.listen(12312);
+
+const server = https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('\n');
+}).listen(8080);
+
+const WebSocket = require('ws');
+//const wss = new WebSocket.Server({ port: 8080 , origin: 'http://tabdn.com'});
+const wss = new WebSocket.Server({ server });
+wss.on('connection', function connection(ws) {
+	var username = '';
+  	ws.on('message', function incoming(message) {
+		var dm = JSON.parse(message);
+		if (dm.operation == 'key'){
+			if (tempKeys[dm.message]){
+				username = tempKeys[dm.message].username;
+			}
+		}
+		else if (dm.type == 'solve'){
+			
+		}
+		
+  	});
+});
+
+
+app.get('/tree',
+	function(req, res){
+		
+		
+		
+		res.write(nunjucks.render('js/treant-js-master/tree.html',{
+			
+		}));
+		res.end();
+	
+    }
+    
+);
+
