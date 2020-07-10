b2c3f4a70af4ee388e3f26fd830be73b3a44ea31
triplelog <robmwilc@gmail.com>
2020-07-10 12:12:02 -0400

    first

--- cpp/postfix.cpp
+++ cpp/postfix.cpp
@@ -1845,7 +1845,7 @@ int main () {
 	std::string newPostfix = pfstr;
 	std::string oldPostfix = "";
 	
-	int maxSteps = 5;
+	int maxSteps = 10;
 	while (newPostfix != oldPostfix && maxSteps >=0){
 		auto t3 = std::chrono::high_resolution_clock::now();
 		newPostfix = removeBracketsOne(newPostfix);
