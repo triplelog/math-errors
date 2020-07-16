6f78096c2f81e68be33289db1122267cff00c4ed
triplelog <robmwilc@gmail.com>
2020-07-16 12:07:25 -0400

    first

---
+++ cpp/nodehello.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <math.h>
+#include <limits.h>
+#include <time.h>
+#include <string.h> 
+#include <ctype.h>
+#include <cmath>
+#include <iostream>
+#include <variant>
+#include <map>
+#include <numeric>
+#include <chrono>
+#include <thread>
+#include <sstream>
+#include <iostream>
+#include <array>
+#include <vector>
+
+
+
+void Hello(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+	//v8::Isolate* isolate = info.GetIsolate();
+	//v8::Local<v8::Context> context = isolate->GetCurrentContext();
+	//int row = info[0]->Int32Value(context).FromJust();
+	//v8::String::Utf8Value s(isolate, info[0]);
+	//std::string str(*s);
+	std::string out("hello world");
+	Nan::MaybeLocal<v8::String> h = Nan::New<v8::String>(out);
+	info.GetReturnValue().Set(h.ToLocalChecked());
+}
+void Init(v8::Local<v8::Object> exports) {
+  v8::Local<v8::Context> context = exports->CreationContext();
+  exports->Set(context,
+               Nan::New("hello").ToLocalChecked(),
+               Nan::New<v8::FunctionTemplate>(Hello)
+                   ->GetFunction(context)
+                   .ToLocalChecked());
+}
+
+NODE_MODULE(helloarray, Init)
