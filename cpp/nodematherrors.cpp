e00ef1a6da9cff537bad1d805b73f7c1562f3c40
triplelog <robmwilc@gmail.com>
2020-07-17 22:12:46 -0400

    first

--- cpp/nodematherrors.cpp
+++ cpp/nodematherrors.cpp
@@ -3110,7 +3110,7 @@ void getAnswerList(std::string s,bool isCorrect) {
 	std::vector<std::string> allStrings; //vector of the next step
 	
 	std::vector<std::string> someStrings = applyRulesVector(newPostfix,isCorrect);
-	std::cout << s << " andand " << someStrings.length() << "\n";
+	std::cout << s << " andand " << someStrings.size() << "\n";
 	for (iii=0;iii<someStrings.size();iii++){
 		std::cout << someStrings[iii] << "\n";
 		someStrings[iii] = removeBracketsOne(someStrings[iii]);
