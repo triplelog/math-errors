b6a4470a9bf72aba434ed85ccd01c9a0b5d8828f
triplelog <robmwilc@gmail.com>
2020-07-21 10:43:58 -0400

    first

--- cpp/nodematherrors.cpp
+++ cpp/nodematherrors.cpp
@@ -1767,15 +1767,15 @@ void makeTree(std::string pfstr){
 					}
 				}
 				if (allListMapBottom.find(s + '@' + t) != allListMapBottom.end()){
-					fullTrees = allListMapFull[s + '@' + t];
-					someBottomTrees = allListMapBottom[s + '@' + t];
+					//fullTrees = allListMapFull[s + '@' + t];
+					//someBottomTrees = allListMapBottom[s + '@' + t];
 					foundFullTrees = true;
 					inStr = s + "@" + t;
 					break;
 				}
 			}
 			
-			if (foundFullTrees){
+			if (foundFullTrees && 3 == 2){
 				listMap[inStr]=fullTrees;
 				someBottomTrees = allListMapBottom[inStr];
 				sbtSz = someBottomTrees.size();
@@ -2779,6 +2779,7 @@ std::vector<std::string> applyRulesVector(std::string userFullString, bool isCor
 					*/
 					
 					std::string tempTemp = userFullString;
+					//std::cout << bottomTrees[ii][1]
 					tempTemp.replace(std::stoi(bottomTrees[ii][3]),std::stoi(bottomTrees[ii][4])+1,newPostfixSecond);
 					//std::cout << "userFullString: "<< userFullString << "\n";
 					tempTemp.replace(std::stoi(bottomTrees[ii][1]),std::stoi(bottomTrees[ii][2]),newPostfixFirst);
