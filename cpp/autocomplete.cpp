90e55f37f1be7a1c36e2c75b832390ce5a61c303
triplelog <robmwilc@gmail.com>
2020-07-23 17:26:15 -0400

    first

--- cpp/autocomplete.cpp
+++ cpp/autocomplete.cpp
@@ -1,4 +1,4 @@
-std::string inputify(std::string input) {
+std::string latexOne(std::string input) {
 
 	int i; int ii; int iii; int idx = 0;
 	bool startOperands = false;
@@ -29,7 +29,6 @@ std::string inputify(std::string input) {
 	flat_hash_map<int,std::string> operandMap;
 	std::string lastInput = "";
 	for (i=0;i<pfstr.length();i++){
-		std::cout << "i: " << i << " and " << pfstr << "\n";
 		if (pfstr.at(i) == '@'){
 			break;
 		}
@@ -82,7 +81,6 @@ std::string inputify(std::string input) {
 				
 				
 			}
-			std::cout << "here\n";
 			std::string fullStr = firstStr + secondStr + pfstr.at(i) + '@' + firstTtr + secondTtr;
 			
 			std::string s = "";
@@ -102,7 +100,7 @@ std::string inputify(std::string input) {
 						}
 						else {
 							if (prec[lastOpMap[child]] < 100){
-								s += "\\\\left("+listMap[child]+"\\\\right)";
+								s += "("+listMap[child]+")";
 							}
 							else {
 								s += listMap[child];
@@ -112,10 +110,10 @@ std::string inputify(std::string input) {
 					}
 					case -69: {
 						if (ii > 0){
-							s += listMap[child]+"\\\\right]";
+							s += listMap[child]+"]";
 						}
 						else {
-							s += "\\\\frac{d}{d"+listMap[child]+"}\\\\left[";
+							s += "\\\\frac{d}{d"+listMap[child]+"}[";
 						}
 						break;
 			
@@ -134,58 +132,58 @@ std::string inputify(std::string input) {
 						s += "|"+listMap[child]+"|";
 						break;
 					case -64:
-						s += "\\\\sin\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\sin("+listMap[child]+")";
 						break;
 					case -63:
-						s += "\\\\cos\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\cos("+listMap[child]+")";
 						break;
 					case -62:
-						s += "\\\\tan\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\tan("+listMap[child]+")";
 						break;
 					case -61:
-						s += "\\\\csc\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\csc("+listMap[child]+")";
 						break;
 					case -60:
-						s += "\\\\sec\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\sec("+listMap[child]+")";
 						break;
 					case -59:
-						s += "\\\\cot\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\cot("+listMap[child]+")";
 						break;
 					case -32:
-						s += "\\\\sin^{-1}\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\sin^(-1)("+listMap[child]+")";
 						break;
 					case -31:
-						s += "\\\\cos^{-1}\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\cos^(-1)("+listMap[child]+")";
 						break;
 					case -30:
-						s += "\\\\tan^{-1}\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\tan^(-1)("+listMap[child]+")";
 						break;
 					case -29:
-						s += "\\\\csc^{-1}\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\csc^(-1)("+listMap[child]+")";
 						break;
 					case -28:
-						s += "\\\\sec^{-1}\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\sec^(-1)("+listMap[child]+")";
 						break;
 					case -27:
-						s += "\\\\cot^{-1}\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\cot^(-1)("+listMap[child]+")";
 						break;
 					case -16:
-						s += "\\\\text{sinh}\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\text{sinh}("+listMap[child]+")";
 						break;
 					case -15:
-						s += "\\\\text{cosh}\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\text{cosh}("+listMap[child]+")";
 						break;
 					case -14:
-						s += "\\\\text{tanh}\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\text{tanh}("+listMap[child]+")";
 						break;
 					case -13:
-						s += "\\\\text{csch}\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\text{csch}("+listMap[child]+")";
 						break;
 					case -12:
-						s += "\\\\text{sech}\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\text{sech}("+listMap[child]+")";
 						break;
 					case -11:
-						s += "\\\\text{coth}\\\\left("+listMap[child]+"\\\\right)";
+						s += "\\\\text{coth}("+listMap[child]+")";
 						break;
 					case -67:
 						s += "\\\\sqrt{"+listMap[child]+"}";
@@ -203,7 +201,7 @@ std::string inputify(std::string input) {
 					case -93: {
 						if (ii > 0){
 							if (prec[lastOpMap[child]] < 100){
-								s += "\\\\left("+listMap[child]+"\\\\right)";
+								s += "("+listMap[child]+")";
 							}
 							else {
 								s += listMap[child];
@@ -298,7 +296,6 @@ std::string inputify(std::string input) {
 			
 			listMap[fullStr]=s;
 			lastOpMap[fullStr]=pfstr.at(i);
-			std::cout << "fullStr: " << fullStr << " and " << s << "\n";
 			lastInput = s;
 			
 		}
@@ -307,7 +304,325 @@ std::string inputify(std::string input) {
 			operandMap[i]=std::to_string(idx);
 			idx++;
 		}
-		std::cout << "i: " << i << " and " << pfstr << "\n";
+		
+	}
+	
+	std::cout << lastInput << "\n";
+	return lastInput;
+
+
+}
+std::string inputify(std::string input) {
+
+	int i; int ii; int iii; int idx = 0;
+	bool startOperands = false;
+	std::string currentOperator = "";
+	flat_hash_map<int,std::string> originalMap;
+	int iidx = 0;
+	std::string pfstr = input;
+	
+	for (i=0;i<pfstr.length();i++){
+		if (pfstr.at(i) == '@'){
+			startOperands = true;
+		}
+		else if (startOperands){
+			if (pfstr.at(i) == '_'){
+				originalMap[iidx] = currentOperator;
+				iidx++; 
+				currentOperator = "";
+			}
+			else {
+				currentOperator += pfstr.at(i);
+			}
+		}
+	}
+	
+	flat_hash_map<std::string,std::string> listMap;
+	flat_hash_map<std::string,char> lastOpMap;
+	
+	flat_hash_map<int,std::string> operandMap;
+	std::string lastInput = "";
+	for (i=0;i<pfstr.length();i++){
+		if (pfstr.at(i) == '@'){
+			break;
+		}
+		else if (pfstr.at(i) != '#'){
+			std::string secondStr = "";
+			std::string secondTtr = "";
+			std::string secondChild = "";
+			int maxi = i-1;
+			
+			for (ii=0;ii<i;ii++){
+				std::string s = "";
+				std::string t = "";
+				for (iii=ii;iii<i;iii++){
+					s += pfstr.at(iii);
+					if (pfstr.at(iii) == '#'){
+						t += operandMap[iii] + '_';
+					}
+				}
+				if (listMap.find(s + '@' + t) != listMap.end()){
+					secondStr = s;
+					secondTtr = t;
+					secondChild = s + '@' + t;
+					maxi = ii;
+					break;
+				}
+			}
+			std::string firstStr = "";
+			std::string firstTtr = "";
+			std::string firstChild = "";
+			std::vector<std::string> fullTrees;
+			
+			if (pfstr.at(i) != '-' && pfstr.at(i) != '/' && (pfstr.at(i) >= 0 || pfstr.at(i) <= -69 )){ // Is at least binary function
+				
+				for (ii=0;ii<maxi;ii++){
+					std::string s = "";
+					std::string t = "";
+					for (iii=ii;iii<maxi;iii++){
+						s += pfstr.at(iii);
+						if (pfstr.at(iii) == '#'){
+							t += operandMap[iii] + '_';
+						}
+					}
+					if (listMap.find(s + '@' + t) != listMap.end()){
+						firstStr = s;
+						firstTtr = t;
+						firstChild = s + '@' + t;
+						break;
+					}
+				}
+				
+				
+			}
+			std::string fullStr = firstStr + secondStr + pfstr.at(i) + '@' + firstTtr + secondTtr;
+			
+			std::string s = "";
+			for (ii=0;ii<2;ii++){
+				std::string child = secondChild;
+				if (ii==0 && firstChild != ""){
+					child = firstChild;
+				}
+				else if (ii==1 && firstChild == ""){
+					break;
+				}
+				switch (pfstr.at(i)){
+					case '^': {
+						if (ii > 0){
+							s += "^";
+							if (prec[lastOpMap[child]] < 100){
+								s += "("+listMap[child]+")";
+							}
+							else {
+								s += listMap[child];
+							}
+						}
+						else {
+							if (prec[lastOpMap[child]] < 100){
+								s += "("+listMap[child]+")";
+							}
+							else {
+								s += listMap[child];
+							}
+						}
+						break;
+					}
+					case -69: {
+						if (ii > 0){
+							s += listMap[child]+"]";
+						}
+						else {
+							s += "d/d"+listMap[child]+"[";
+						}
+						break;
+			
+					}
+					case -85: {
+						if (ii > 0){
+							s.replace(6,0,listMap[child]+" d");
+						}
+						else {
+							s += "int "+listMap[child]+"}";
+						}
+						break;
+			
+					}
+					case -34:
+						s += "|"+listMap[child]+"|";
+						break;
+					case -64:
+						s += "sin("+listMap[child]+")";
+						break;
+					case -63:
+						s += "cos("+listMap[child]+")";
+						break;
+					case -62:
+						s += "tan("+listMap[child]+")";
+						break;
+					case -61:
+						s += "csc("+listMap[child]+")";
+						break;
+					case -60:
+						s += "sec("+listMap[child]+")";
+						break;
+					case -59:
+						s += "cot("+listMap[child]+")";
+						break;
+					case -32:
+						s += "sin^(-1)("+listMap[child]+")";
+						break;
+					case -31:
+						s += "cos^(-1)("+listMap[child]+")";
+						break;
+					case -30:
+						s += "tan^(-1)("+listMap[child]+")";
+						break;
+					case -29:
+						s += "csc^(-1)("+listMap[child]+")";
+						break;
+					case -28:
+						s += "sec^(-1)("+listMap[child]+")";
+						break;
+					case -27:
+						s += "cot^(-1)("+listMap[child]+")";
+						break;
+					case -16:
+						s += "sinh("+listMap[child]+")";
+						break;
+					case -15:
+						s += "cosh("+listMap[child]+")";
+						break;
+					case -14:
+						s += "tanh("+listMap[child]+")";
+						break;
+					case -13:
+						s += "csch("+listMap[child]+")";
+						break;
+					case -12:
+						s += "sech("+listMap[child]+")";
+						break;
+					case -11:
+						s += "coth("+listMap[child]+")";
+						break;
+					case -67:
+						s += "sqrt("+listMap[child]+")";
+						break;
+					case -84: {
+						if (ii > 0){
+							s += listMap[child]+")";
+						}
+						else {
+							s += "sqrt["+listMap[child]+"](";
+						}
+						break;
+			
+					}
+					case -93: {
+						if (ii > 0){
+							if (prec[lastOpMap[child]] < 100){
+								s += "("+listMap[child]+")";
+							}
+							else {
+								s += listMap[child];
+							}
+					
+						}
+						else {
+							if (listMap[child] == "e"){
+								s += "ln";
+							}
+							else {
+								s += "log_{"+listMap[child]+"}";
+							}
+						}
+						break;
+			
+					}
+					case '-': {
+						if (prec[pfstr.at(i)] >= prec[lastOpMap[child]]){
+							s += "-("+listMap[child]+")";
+						}
+						else {
+							s += "-"+listMap[child];
+						}
+						break;
+					}
+					case '/': {
+						s += "1/("+listMap[child]+")";
+						/*
+						if (prec[pfstr.at(i)] >= prec[lastOpMap[child]]){
+							s += "/("+listMap[child]+")";
+						}
+						else {
+							s += "/"+listMap[child];
+						}*/
+						break;
+					}
+					default: {
+						if (prec[pfstr.at(i)] > prec[lastOpMap[child]]){
+							if (ii > 0){
+								if (pfstr.at(i) == '*'){
+									s += "*("+listMap[child]+")";
+								}
+								else {
+									s += pfstr.at(i)+"("+listMap[child]+")";
+								}
+							}
+							else {
+								s += "("+listMap[child]+")";
+							}
+						}
+						else if (prec[pfstr.at(i)] == prec[lastOpMap[child]] && pfstr.at(i) != lastOpMap[child]){
+							if (ii > 0){
+								if (pfstr.at(i) == '*'){
+									s += "*"+listMap[child];//want to move this into numerator somehow
+								}
+								else if (pfstr.at(i) == '+'){
+									s += listMap[child];
+								}
+								else {
+									s += pfstr.at(i)+"("+listMap[child]+")";
+								}
+							}
+							else {
+								if (pfstr.at(i) == '*'){
+									s += listMap[child];
+								}
+								else if (pfstr.at(i) == '+'){
+									s += listMap[child];
+								}
+								else {
+									s += "("+listMap[child]+")";
+								}
+							}
+						}
+						else {
+							if (ii > 0){
+								if (pfstr.at(i) == '*'){
+									s += "*"+listMap[child];
+								}
+								else {
+									s += pfstr.at(i)+listMap[child];
+								}
+							}
+							else {
+								s += listMap[child];
+							}
+						}
+					}
+				}
+			}
+			
+			listMap[fullStr]=s;
+			lastOpMap[fullStr]=pfstr.at(i);
+			lastInput = s;
+			
+		}
+		else {
+			listMap["#@" + std::to_string(idx) + "_"]=originalMap[idx];
+			operandMap[i]=std::to_string(idx);
+			idx++;
+		}
 		
 	}
 	
