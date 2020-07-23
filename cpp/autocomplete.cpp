57538dcdff645fc590abe6b2d5ac8a47ff15d7cb
triplelog <robmwilc@gmail.com>
2020-07-23 17:50:35 -0400

    first

--- cpp/autocomplete.cpp
+++ cpp/autocomplete.cpp
@@ -312,6 +312,57 @@ std::string latexOne(std::string input) {
 
 
 }
+
+int lev(std::string ss, std::string st){
+
+    int i,j,m,n,temp,tracker;
+	m = ss.length(); 
+    char s[m + 1]; 
+    strcpy(s, ss.c_str());
+    n = st.length(); 
+    char t[n + 1]; 
+    strcpy(t, st.c_str());
+
+    for(i=0;i<=m;i++){
+    	d[0][i] = i;
+    }
+
+    for(j=0;j<=n;j++){
+    	d[j][0] = j;
+    }
+
+
+    for (j=1;j<=m;j++) {
+
+        for(i=1;i<=n;i++) {
+
+            if(s[i-1] == t[j-1])
+
+            {
+
+                tracker = 0;
+
+            }
+
+            else
+
+            {
+
+                tracker = 1;
+
+            }
+
+            temp = MIN((d[i-1][j]+1),(d[i][j-1]+1));
+
+            d[i][j] = MIN(temp,(d[i-1][j-1]+tracker));
+
+        }
+
+    }
+
+    return d[n][m];
+
+}
 std::string inputify(std::string input) {
 
 	int i; int ii; int iii; int idx = 0;
@@ -634,7 +685,9 @@ std::string inputify(std::string input) {
 void autocomplete(flat_hash_map<std::string,std::vector<std::string>> reverseMap, std::string newPostfix,std::string rawAnswer){
 
 	for (flat_hash_map<std::string,std::vector<std::string>>::iterator iter = reverseMap.begin(); iter != reverseMap.end(); ++iter){
-		std::cout << "comp answer: " << iter->first << " into "<< inputify(iter->first) << "\n";
+		std::string ca = inputify(iter->first);
+		std::cout << "distance: " << lev(ca,rawAnswer) << " of "<< inputify(iter->first) << "\n";
+		
 	}
 	std::cout << "question: " << inputify(newPostfix) << "\n";
 	std::cout << "user answer: " << rawAnswer << "\n";
