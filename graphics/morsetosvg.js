881d7923603d2cda2bb91e926873c40dc7cef37c
triplelog <robmwilc@gmail.com>
2020-07-28 08:23:54 -0400

    nm

--- graphics/morsetosvg.js
+++ graphics/morsetosvg.js
@@ -163,11 +163,11 @@ var svg = '<html><body><svg height="216" width="1080">';
 for (var i=0;i<10;i++){
 	noise = OpenSimplexNoise.makeNoise3D(Date.now());
 	noise2D = OpenSimplexNoise.makeNoise2D(Date.now());
-	var end3 = drawDot({x:15+120*i,y:15,radius:15},4.0,0.2,0.09,.9,3);
+	var end3 = drawDot({x:15+120*i,y:15,radius:15},8.0,0.05,0.09,.9,3);
 	svg += end3;
 	noise = OpenSimplexNoise.makeNoise3D(Date.now());
 	noise2D = OpenSimplexNoise.makeNoise2D(Date.now());
-	var end3 = drawDash({x:75+120*i,y:15,radius:15},4.0,0.075,0.09,.9,3);
+	var end3 = drawDash({x:75+120*i,y:15,radius:15},4.0,0.05,0.09,.9,3);
 	svg += end3;
 }
 /*
