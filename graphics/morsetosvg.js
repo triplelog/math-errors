96e42cdf5d894bc3c532e1dae8db24ab9efbc65e
triplelog <robmwilc@gmail.com>
2020-07-28 08:08:24 -0400

    nm

--- graphics/morsetosvg.js
+++ graphics/morsetosvg.js
@@ -51,7 +51,7 @@ function drawDeformedOval( circle,frequency, magnitude,seed) {
             var radius = circle.radius * (1 + magnitude * deformation);
 			var angAdj = angle % (Math.PI / 2);
 			angAdj = (Math.PI / 4)*(Math.PI / 4) - (angAdj - Math.PI / 4)*(angAdj - Math.PI / 4);
-			angAdj = 1.5 - angAdj;
+			angAdj = 1 + angAdj;
 			console.log(angle,angAdj);
 			//radius *= angAdj;
             // Extend the circle to this deformed radius
