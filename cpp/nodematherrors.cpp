7e61e73a1163634f47b135efc18999b696bf0796
triplelog <robmwilc@gmail.com>
2020-07-29 08:14:30 -0400

    nm

--- cpp/nodematherrors.cpp
+++ cpp/nodematherrors.cpp
@@ -2385,7 +2385,9 @@ bool correctAnswer(std::string s){
 			correctAnswers.push_back(tempCorrect[ii]);
 			std::cout << "correct: " << tempCorrect[ii] << "\n";
 			answerListMap.erase(tempCorrect[ii]);
+			std::cout << "erased: " << tempCorrect[ii] << "\n";
 			std::vector<std::string> v = makeSolutionList(tempCorrect[ii]);
+			std::cout << "made solution list: " << tempCorrect[ii] << "\n";
 			//std::cout << "len of sol: " << v.size() << "\n";
 		}
 
