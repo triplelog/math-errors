fa9b88a077add2c4333d49b336ad9c647ef93a23
triplelog <robmwilc@gmail.com>
2020-07-31 09:24:34 -0400

    nm

--- python/elomap.py
+++ python/elomap.py
@@ -4,5 +4,5 @@ outstr = "flat_hash_map<int,int> eloMap; "
 for rr in range(1,100):
 	rrr = (100.0-rr)/rr
 	rrrr = round(1000*math.log(rrr)/math.log(10))
-	outstr += "eloMap["+rr+"]="+rrrr+"; "
+	outstr += "eloMap["+str(rr)+"]="+str(rrrr)+"; "
 print(outstr)
