881d814d0a71fe80e64509ec461563335f5da391
triplelog <robmwilc@gmail.com>
2020-08-06 12:08:51 -0400

    nm

--- cpp/postfixify.cpp
+++ cpp/postfixify.cpp
@@ -165,6 +165,7 @@ std::string replaceFunctions(std::string input_str){
 	flat_hash_map<std::string,std::string> replacements7;
 	flat_hash_map<std::string,std::string> replacements8;
 	
+	flat_hash_map<std::string,std::string> query7;
 	flat_hash_map<std::string,std::string> query4;
 	flat_hash_map<std::string,std::string> query3;
 	flat_hash_map<std::string,std::string> query2;
