9b3724aafc70f1d2279e83aad96d7cbfc1a6b43b
triplelog <robmwilc@gmail.com>
2020-08-07 08:08:57 -0400

    nm

--- cpp/parallelmain.cpp
+++ cpp/parallelmain.cpp
@@ -3451,7 +3451,7 @@ void GetAnswers(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 	correctSolutionList.clear();
 	incorrectSolutionList.clear();
 	answerMap.clear();
-	maxSteps = 10;
+	maxSteps = 25;
 	
 	auto a1 = std::chrono::high_resolution_clock::now();
 	maxFound = 0;
