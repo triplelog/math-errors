d7c66fdd3b2a77de4028ad89b9c9223fc0d1c592
triplelog <robmwilc@gmail.com>
2020-08-18 11:41:59 -0400

    nm

--- mathdocs/rule-maker-macro.js
+++ mathdocs/rule-maker-macro.js
@@ -4,6 +4,7 @@ module.exports = function (registry) {
     self.process(function (parent, target, attrs) {
       var text;
       console.log(JSON.stringify(attrs));
+      console.log(target);
       text = target;
       return self.createInline(parent, 'quoted', text, { 'type': 'strong' }).convert()
     })
