814b67fb54efcaeba1d55c19910ef2b50c2d7411
triplelog <robmwilc@gmail.com>
2020-08-18 15:36:51 -0400

    nm

---
+++ mathdocs/tree-maker-macro.js
@@ -0,0 +1,11 @@
+module.exports = function (registry) {
+  registry.block(function () {
+    var self = this
+    self.named('tree')
+    self.onContext('paragraph')
+    self.process(function (parent, reader) {
+      var lines = reader.getLines().map(function (l) { return l.toUpperCase() })
+      return self.createBlock(parent, 'paragraph', lines)
+    })
+  })
+}
