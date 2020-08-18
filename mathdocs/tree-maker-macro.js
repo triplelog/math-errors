77f111986885c6a844325fa4f4b96bb5dbe4d1e8
triplelog <robmwilc@gmail.com>
2020-08-18 16:25:31 -0400

    nm

--- mathdocs/tree-maker-macro.js
+++ mathdocs/tree-maker-macro.js
@@ -11,7 +11,7 @@ module.exports = function (registry) {
       	cards.push(lines[i]);
       }
       console.log(JSON.stringify(cards));
-      var blk = self.createBlock(parent, 'tree', "",{source:cards});
+      var blk = self.createBlock(parent, 'tree', "",{source:cards},{source:cards});
       return blk;
     })
   })
