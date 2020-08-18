fb25a6f5b52077bd29d17077cb540ce2ed8de526
triplelog <robmwilc@gmail.com>
2020-08-18 16:10:12 -0400

    nm

--- mathdocs/tree-maker-macro.js
+++ mathdocs/tree-maker-macro.js
@@ -5,10 +5,12 @@ module.exports = function (registry) {
     self.onContext('paragraph')
     self.process(function (parent, reader) {
       var cards = [];
-      var lines = reader.lines
+      var lines = reader.lines;
+      console.log(JSON.stringify(lines));
       for (var i = 0; i < lines.length; i++) {
       	cards.push(lines[i]);
       }
+      console.log(JSON.stringify(cards));
       var blk = self.createBlock(parent, 'tree', cards);
       return blk;
     })
