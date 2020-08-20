38eaba39fefb6a2d7292d09b32e580eb5aa16200
triplelog <robmwilc@gmail.com>
2020-08-20 15:31:39 -0400

    nm

--- main-server.js
+++ main-server.js
@@ -550,8 +550,6 @@ example::3^2+4[]
 		const html = asciidoctor.convert('this is a $A^12+B/7$ for real with more $x=7$ to come.\n'+instruction, { 'extension_registry': registry, safe: 'safe', backend: 'html5', template_dir: './templates' });
 		//console.log(html);
 		//SubjectData.find({}, function(err,result) {
-			console.log("errrr", err);
-			console.log("res", result);
 			res.write(nunjucks.render('createlesson.html',{
 				info: info,
 				correct:correct,
