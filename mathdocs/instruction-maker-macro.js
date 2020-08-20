547dfcaa84937d121b3960f5017de787a7f0bc7d
triplelog <robmwilc@gmail.com>
2020-08-20 13:05:42 -0400

    nm

--- mathdocs/instruction-maker-macro.js
+++ mathdocs/instruction-maker-macro.js
@@ -3,7 +3,9 @@ const assert = require('assert');
 const bindingP = require.resolve(`../build/Release/bindingP`);
 const maincppp = require(bindingP);
 var retHelloP = maincppp.hello();
-
+const asciidoctor = require('asciidoctor')();
+const registry = asciidoctor.Extensions.create();
+require('./mathdocs/rule-maker-macro.js')(registry);
 
 module.exports = function (registry,filen) {
   maincppp.makelesson(filen);
@@ -12,29 +14,40 @@ module.exports = function (registry,filen) {
     self.named('rule')
     self.onContext('paragraph')
     self.process(function (parent, reader) {
-      var cards = {constraints:[]};
+      var cards = {constraints:[],explanation:""};
       var lines = reader.lines;
+      var isText = false;
       for (var i = 0; i < lines.length; i++) {
-      	if (lines[i].substr(0,4).toLowerCase() == "in: "){
-      		var input = maincppp.latexify(lines[i].substr(4));
-		  	var html = katex.renderToString(input, {
-				throwOnError: false
-		  	});
-      		cards['in']=html;
+      	if (!isText){
+			if (lines[i].substr(0,4).toLowerCase() == "in: "){
+				var input = maincppp.latexify(lines[i].substr(4));
+				var html = katex.renderToString(input, {
+					throwOnError: false
+				});
+				cards['in']=html;
+			}
+			else if (lines[i].substr(0,5).toLowerCase() == "out: "){
+				var html = katex.renderToString(maincppp.latexify(lines[i].substr(5)), {
+					throwOnError: false
+				});
+				cards['out']=html;
+			}
+			else if (lines[i].substr(0,2).toLowerCase() == "--"){
+				isText = true;
+			}
+			else{
+				var html = katex.renderToString(maincppp.latexify(lines[i]), {
+					throwOnError: false
+				});
+				cards['constraints'].push(html);
+			}
       	}
-      	else if (lines[i].substr(0,5).toLowerCase() == "out: "){
-      		var html = katex.renderToString(maincppp.latexify(lines[i].substr(5)), {
-				throwOnError: false
-		  	});
-      		cards['out']=html;
-      	}
-      	else{
-      		var html = katex.renderToString(maincppp.latexify(lines[i]), {
-				throwOnError: false
-		  	});
-      		cards['constraints'].push(html);
+      	else {
+      		//render the asciidoc
+      		cards['explanation']+= lines[i]+"\n";
       	}
       }
+      cards['explanation']= asciidoctor.convert(cards['explanation'],{ 'extension_registry': registry, safe: 'safe', backend: 'html5', template_dir: './templates' });
       var blk = self.createBlock(parent, 'instruction', "",{cards:cards,type:"rule"});
       return blk;
     })
