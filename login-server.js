2a847a38e637db86b6cb68bd8cd4bd1f5d612f1f
triplelog <robmwilc@gmail.com>
2020-08-20 18:35:23 -0400

    nm

--- login-server.js
+++ login-server.js
@@ -21,9 +21,9 @@ const User = require('./models/user');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://matherrors:kZGWGda3A@localhost:27017/matherrors?authSource=matherrors', {useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true });
 
-var schema = new mongoose.Schema({subj: String, nam: String});
+var schema = new mongoose.Schema({"subj": String, "nam": String});
 var Tankk = mongoose.model('Tankk', schema);
-var dog = new Tankk({subj:"a",nam:"b"});
+var dog = new Tankk({"subj":"a","nam":"b"});
 dog.save(function(err,result){
 	console.log("err1: ",err);
 	console.log("res2: ",result);
