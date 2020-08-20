f53f84cf1a0c6d14289356ab1ae25281b2c70086
triplelog <robmwilc@gmail.com>
2020-08-20 18:22:11 -0400

    nm

--- login-server.js
+++ login-server.js
@@ -21,6 +21,17 @@ const User = require('./models/user');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://matherrors:kZGWGda3A@localhost:27017/matherrors', {useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true });
 
+var schema = new mongoose.Schema({subject: String, name: String});
+var Tank = mongoose.model('Tank', schema);
+var dog = new Tank({subject:"a",name:"b"});
+dog.save(function(err,result){
+	console.log("err1: ",err);
+	console.log("res2: ",result);
+	Tank.countDocuments({}, function(err,result){
+		console.log("res3: ",result);
+	});
+});
+
 
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy;
