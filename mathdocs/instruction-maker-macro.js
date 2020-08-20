8fb0afbb9d46ad87a0ca7a70b7aed3db6e39ba7f
triplelog <robmwilc@gmail.com>
2020-08-19 21:18:18 -0400

    nm

--- mathdocs/instruction-maker-macro.js
+++ mathdocs/instruction-maker-macro.js
@@ -75,6 +75,8 @@ module.exports = function (registry) {
     	
       var jsonmessage = maincppp.onerule(target);
       console.log(jsonmessage);
+      jsonmessage = jsonmessage.replace(/\\/g,"\\");
+      console.log(jsonmessage);
 	  var parsed = JSON.parse(jsonmessage);
 	  for (var i=0;i<parsed.length;i++){
 	  	parsed[i].start = katex.renderToString(parsed[i].start, {throwOnError: false});
