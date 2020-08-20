9bc09c5aae0f844b20d0ff86e799132a84fd6aa5
triplelog <robmwilc@gmail.com>
2020-08-20 15:38:10 -0400

    nm

--- models/subjects.js
+++ models/subjects.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const SubjectData = new Schema({subject:String,topics:{}});
+const SubjectData = new Schema({subject:String,topics:{type: Schema.Types.Mixed, required: true}});
 module.exports = mongoose.model('SubjectData', SubjectData);
