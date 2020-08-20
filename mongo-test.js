40bf91926bd72982ee49a70f7e8f7fc4542b5a6e
triplelog <robmwilc@gmail.com>
2020-08-20 18:41:30 -0400

    nm

--- mongo-test.js
+++ mongo-test.js
@@ -1,6 +1,23 @@
 
 const https = require('https');
+//const http = require('http');
+var fs = require("fs");
+//var myParser = require("body-parser");
+var qs = require('querystring');
+const { exec } = require('child_process');
+const bodyParser = require('body-parser');
+var parse = require('csv-parse');
+var nunjucks = require('nunjucks');
+var crypto = require("crypto");
+//const flate = require('wasm-flate');
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/matherrors.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/matherrors.com/fullchain.pem')
+};
+const { PerformanceObserver, performance } = require('perf_hooks');
 
+var tempKeys = {};
+const User = require('./models/user');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://matherrors:kZGWGda3A@localhost:27017/matherrors?authSource=matherrors', {useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true });
 
