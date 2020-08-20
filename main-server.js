56ff2a12a35d567ade429666bb7a045043b7e03b
triplelog <robmwilc@gmail.com>
2020-08-20 16:16:23 -0400

    nm

--- main-server.js
+++ main-server.js
@@ -242,7 +242,7 @@ wss.on('connection', function connection(ws) {
 					console.log("sub: ",subject);
 					var topics = {};
 					topics[topic]=[{slug:slug,lesson:lesson}];
-					var subjectData = new SubjectData({subject:subject});
+					var subjectData = new SubjectData({subject:"subject"});
 					subjectData.save(function(err,result){
 						console.log("res: ",JSON.stringify(result));
 						if (err){
