eacbd1179bc0c8d9e3c72acb0c2d167213cbc4da
triplelog <robmwilc@gmail.com>
2020-08-20 20:46:01 -0400

    nm

--- main-server.js
+++ main-server.js
@@ -567,6 +567,8 @@ example::3^2+4[]
 	const asciidoctor = require('asciidoctor')();
 	const registry = asciidoctor.Extensions.create();
 	console.log(Object.keys(global));
+	delete Opal;
+	console.log(Object.keys(global));
 	//require('./mathdocs/rule-maker-macro.js')(registry);
 	
     	//require('./mathdocs/instruction-maker-macro.js')(registry,instruction);
