adc16a9d9d7954bae3683e10dcb7e1918ecb624a
triplelog <robmwilc@gmail.com>
2020-08-20 20:15:57 -0400

    nm

--- main-server.js
+++ main-server.js
@@ -36,7 +36,9 @@ console.log(Object.getOwnPropertyNames(global));
 console.log(Object.keys(global));
 const {NodeVM, VMScript} = require('vm2');
 const vm = new NodeVM({
-    require: ['katex','asciidoctor']
+    require: {
+    	import: ['katex','asciidoctor']
+    }
 });
 const script = new VMScript("const katex = require('katex'); const asciidoctor = require('asciidoctor')();");
 vm.run(script);
