8701720dce97501a5f61e112392e98e7806550dc
triplelog <robmwilc@gmail.com>
2020-08-20 21:52:06 -0400

    nm

--- main-server.js
+++ main-server.js
@@ -52,8 +52,8 @@ var mdoptions = {
 };
 var md = new markdown();
 md.use(require('@gerhobbelt/markdown-it-container'), 'rule' , mdoptions);
-console.log(md.render('::: spoiler click me\n*content*\n:::\n'));
-
+console.log(md.render('::: rule click me\n*content*\n:::\n'));
+console.log(bre);
 //const asciidoctor = new require('asciidoctor')();
 //const registry = asciidoctor.Extensions.create();
 
