f675df696b61feb8b5122e2452ed6f1f8d18f4ed
triplelog <robmwilc@gmail.com>
2020-08-20 22:07:48 -0400

    nm

---
+++ markdown-it-rules.js
@@ -0,0 +1,60 @@
+var mdoptions = {
+  validate: function(params) {
+    return params.trim().match(/^rule\s+(.*)$/);
+  },
+ 
+  render: function (tokens, idx) {
+    var m = tokens[idx].info.trim().match(/^rule\s+(.*)$/);
+ 
+    if (tokens[idx].nesting === 1) {
+      // opening tag
+      return '<details><summary>' + md.utils.escapeHtml(m[1]) + '</summary>\n';
+ 
+    } else {
+      // closing tag
+      return '</details>\n';
+    }
+  },
+  
+  content: function (tokens, idx) {
+  	  console.log("CCC:",tokens[idx].markup);
+  	  var cards = {constraints:[],explanation:""};
+      var lines = reader.lines;
+      var isText = false;
+      for (var i = 0; i < lines.length; i++) {
+      	if (!isText){
+			if (lines[i].substr(0,4).toLowerCase() == "in: "){
+				var input = maincppp.latexify(lines[i].substr(4));
+				var html = katex.renderToString(input, {
+					throwOnError: false
+				});
+				cards['in']=html;
+			}
+			else if (lines[i].substr(0,5).toLowerCase() == "out: "){
+				var html = katex.renderToString(maincppp.latexify(lines[i].substr(5)), {
+					throwOnError: false
+				});
+				cards['out']=html;
+			}
+			else if (lines[i].substr(0,3) == "eee"){
+				isText = true;
+			}
+			else{
+				var html = katex.renderToString(maincppp.latexify(lines[i]), {
+					throwOnError: false
+				});
+				cards['constraints'].push(html);
+			}
+      	}
+      	else {
+      		//render the asciidoc
+      		cards['explanation']+= lines[i]+"\n";
+      	}
+      }
+      if (cards['explanation'].length > 0){
+      	cards['explanation']= asciidoctor2.convert(cards['explanation'],{ 'extension_registry': registry2, safe: 'safe', backend: 'html5', template_dir: '../templates' });
+      }
+  }
+};
+module.exports = mdoptions;
+
