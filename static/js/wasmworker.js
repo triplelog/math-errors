9db955585f56f9e1923f629c51d2a95496850950
triplelog <robmwilc@gmail.com>
2020-08-30 09:59:25 -0400

    nm

--- static/js/wasmworker.js
+++ static/js/wasmworker.js
@@ -1,1 +1,22 @@
 importScripts('../wasmhello.js');
+
+var l = Module.cwrap("LatexIt","string",["string"]);
+var p = Module.cwrap("PlotIt","string",["string","number","number","number"]);
+
+var latex = "";
+function addLatex(x) {
+	latex += x;
+}
+	
+var svg = "";
+function addSVG(x) {
+	svg += x;
+}
+
+
+onmessage = function(e) {
+  console.log('Message received from main script');
+  var workerResult = 'Result: ' + (e.data);
+  console.log('Posting message back to main script');
+  postMessage(workerResult);
+}
