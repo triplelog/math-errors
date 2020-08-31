215f5ccab1cd2af59643d673d29bfb2f581cba20
triplelog <robmwilc@gmail.com>
2020-08-31 10:38:29 -0400

    nm

--- static/wasmworker.js
+++ static/wasmworker.js
@@ -2,6 +2,7 @@ importScripts('wasmhello.js');
 
 var l = Module.cwrap("LatexIt","string",["string"]);
 var p = Module.cwrap("PlotIt","string",["string","number","number","number"]);
+var a = Module.cwrap("AddRules","string",["string","string"]);
 
 var latex = "";
 function addLatex(x) {
@@ -22,11 +23,15 @@ onmessage = function(e) {
 		l(message[1]);
 		result = ["latex",message[1],latex];
 	}
-	else {
+	else if (message[0] == "plot"){
 		svg = "";
 		p(message[1],message[2],message[3],message[4],message[5]);
 		svg += '<input type="range" id="domainSlider" min="0" max="'+(message[6]*2)+'" value="'+message[6]+'"></input>';
 		result = ["svg",message[1],svg];
+	}
+	else if (message[0] == "rules"){
+		a(message[1],message[2]);
+		result = ["rules",message[1],"done"];
 	}
 	postMessage(result);
 }
